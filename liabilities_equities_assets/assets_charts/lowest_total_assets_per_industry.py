ae6c0f9c38fdc3afd5daf80b4e032ec43e13ee6e
Sasa Djukic <djukicmsasa@gmail.com>
2023-03-25 12:20:07 +0100

    lowest total assets per industry

---
+++ liabilities_equities_assets/assets_charts/lowest_total_assets_per_industry.py
@@ -0,0 +1,88 @@
+
+
+import sys 
+import matplotlib.pyplot as plt
+import pandas as pd  
+
+file_path = r'C:\Users\sasad\Desktop\github\nz_gov_data\liabilities_equities_assets'
+sys.path.append(file_path)
+
+from total_assets import lowest_api 
+
+def display_highest_total_assets(lpi: pd.Series) -> None:
+    
+    industry = lpi.index.tolist()
+    assets = lpi.values.tolist()
+
+    shorten_names = [
+        'Public Order, Safety',
+        'Education and Training',
+        'Healthcare',
+        'Media and Telecommunications',
+        'Arts, Recreation'
+    ]
+
+    industry[:] = [x for x in shorten_names]
+
+    line_color = '#D83A56'
+    color = '#0E185F'
+    bg_color = '#C9EEFF'
+    text_color = '#FF5677'
+
+    fig, ax = plt.subplots()
+    plt.bar(industry, assets, color = color)
+
+    fig.patch.set_facecolor(bg_color)
+    ax.set_facecolor(bg_color)
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_color(color)
+    ax.spines['left'].set_color(color)
+
+    plt.suptitle(
+        'NEW ZEALAND LOWEST TOTAL ASSETS PER INDUSTRY 2013-2021',
+        fontfamily = 'Bahnschrift',
+        fontsize = 18,
+        color = text_color
+    )
+
+    plt.title(
+        '*source https://www.data.govt.nz/',
+        fontfamily = 'Bahnschrift',
+        color = text_color
+    )
+
+    plt.ylabel(
+        'ASSETS (in millions of NZ Dollars)',
+        fontfamily = 'Bahnschrift',
+        fontsize = 12.5,
+        color = text_color
+    )
+
+    plt.xlabel(
+        'INDUSTRY NAME',
+        fontfamily = 'Bahnschrift',
+        fontsize = 12.5,
+        color = text_color
+    )
+
+    plt.xticks(color=color)
+    plt.yticks(color=color)
+
+    for index, value in enumerate(assets):
+        plt.text(
+            index,
+            value, 
+            str(value),
+            ha = 'center',
+            position = (index, value-7_500),
+            fontweight = 'bold',
+            color = 'w',
+            fontsize = 15
+        )
+
+    plt.show()
+
+display_highest_total_assets(lowest_api)
+
